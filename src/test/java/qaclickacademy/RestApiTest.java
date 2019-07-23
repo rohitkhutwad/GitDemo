d1764ebdf70c077b88a76d6aae7f2ce06d7c668a
rohit <khutwadr@gmail.com>
2019-07-23 13:01:29 +0530

    asian commit

--- src/test/java/qaclickacademy/RestApiTest.java
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -14,6 +14,8 @@ public class RestApiTest {
 		System.out.println("RestApisix");
 		System.out.println("Demo1");
 		System.out.println("Demo2");
+		System.out.println("Demo3");
+		System.out.println("Demo4");
 
 	}
 	
